d7e3cb6bdaa953b6c1005ab03eb89655f21c261f
codingFer <fernando_rojas2014@hotmail.com>
2020-01-29 17:34:58 -0400

    Updates

---
+++ precache-manifest.857906c075ad5254388745f2d987e49e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80031aa1a3d2167565f07089904ce17d",
+    "url": "/Redux-for-bedu/index.html"
+  },
+  {
+    "revision": "2bba03d183bac7b71175",
+    "url": "/Redux-for-bedu/static/css/main.1b9a2029.chunk.css"
+  },
+  {
+    "revision": "918ebfbab1507c636843",
+    "url": "/Redux-for-bedu/static/js/2.a297d29c.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/Redux-for-bedu/static/js/2.a297d29c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2bba03d183bac7b71175",
+    "url": "/Redux-for-bedu/static/js/main.13af88b7.chunk.js"
+  },
+  {
+    "revision": "c82f83b160f292f5e322",
+    "url": "/Redux-for-bedu/static/js/runtime-main.36b4d42c.js"
+  }
+]);
